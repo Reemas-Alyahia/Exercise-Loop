22aa82bfa55590ae5f9ff776e9031ad2bdb75343
Reemas Alyahia <130400514+mime2001@users.noreply.github.com>
2024-10-23 20:11:03 +0300

    Add files via upload

---
+++ Exercise _Loop/src/Main.java
@@ -0,0 +1,125 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+//        for (int i = 0; i < 100; i++) {
+//
+//            if ( i % 3 == 0&& i % 5 == 0) {
+//                System.out.println("instaed of " + i + " is FizzBuzz");
+//            } else if (i % 5 == 0) {
+//                System.out.println("instaed of " + i + " is Buzz");
+//            } else if (i % 3 == 0) {
+//                System.out.println("instaed of " + i + " is Fizz");
+//            } }
+////////////////////////////2th////////////////////////
+//            Write a Java program to reverse a string.
+//            Test Data: Input a string: The quick brown fox Expected Output: Reverse
+//            string: xof nworb kciuq ehT
+            Scanner input = new Scanner(System.in);
+//  String revers_words;
+// do {
+//     System.out.println("Enter words and the program well revers them and write stop finish process");
+//     revers_words=input.nextLine();
+//     StringBuilder s =new StringBuilder(revers_words);
+//     System.out.println("The  reverse string is : "+s.reverse());
+// }while (!revers_words.equals("stop"));
+//        System.out.println("The program is finish");
+
+
+            ////////////////////////3th///////////
+
+
+//        int number;
+//        int fact_number = 1;
+//
+//        System.out.print("Enter any positive integer: ");
+//        number= input.nextInt();
+//
+//        for(int i=1; i<=number; i++)
+//        {
+//            fact_number =fact_number*i;
+//        }
+//
+//        System.out.println( fact_number);
+
+////////////////////////4th/////////////////
+//        int firstnumber,secondnumber;
+//
+//        int result = 1;
+//
+//        System.out.print("Enter the first number ");
+//        firstnumber = input.nextInt();
+//        System.out.print("now Enter the second number ");
+//        secondnumber = input.nextInt();
+//
+//        for(int i = 1; i <= secondnumber; i++)
+//        {
+//            result =result * firstnumber;
+//        }
+//
+//        System.out.println(result);
+            /////////////////////////6th////////////////
+//
+//              int num ;
+//              int sumeven=0;
+//              int sumodd=0;
+//          do{
+//              System.out.println("please enter any number you want or 0 stop");
+//          num =input.nextInt();
+//           if(num%2==0)
+//           {sumeven=sumeven+num;}
+//           else sumodd=sumodd+num;}   while (num!=0);
+//        System.out.println("the even is:  "+sumeven);
+//        System.out.println("the odd is:   "+sumodd);
+            /////////////////6th///////////////////////
+//        Write a program that prompts the user to input a positive integer. It
+//        should then output a message indicating whether the number is a prime
+//        number.
+//
+//        System.out.print("Enter the positive integer ");
+//        int prim_number = input.nextInt();
+//
+//        boolean prim= true;
+//
+//        for(int i = 2; i < prim_number; i++)
+//            {if(prim_number % i == 0) { prim = false; break; }}
+//        ///if the number is true and the number must be more than 1 !
+//        if(prim && prim_number > 1) {
+//            System.out.println(" Greet the number is prime");}
+//        else
+//        { System.out.println("No the number is not");}
+            //////////////////////7th///////////////
+//        7.Use a for loop to print headings for four weeks (Weeks 1 - 4). Then use
+//        another for loop to print the days (Days 1 -7) for each week.
+//        int week; int day;// nasted for
+//        for (week = 1; week <=4 ; week++) {
+//            System.out.println ("weak "+week);
+//            for(day=1;day<=7;day++){
+//                System.out.println ( "Day"+day);
+//            }
+//        }
+        //////////////////////8th///////////////
+//        Write a program thats check if the word is a palindrome or not.hint:A
+//        string is said to be a palindrome if it is the same if we start reading it from
+//        left to right or right to left.
+//        System.out.println("Enter word and the program well check if the word is palindrome or not and but # to stop");
+//        String word=input.nextLine();
+//        StringBuilder s =new StringBuilder(word);
+//        s.reverse();
+//            if (word.equalsIgnoreCase(s.toString())) {
+//                System.out.println("yes it is palindrome word");
+//            } else System.out.println("no it's not palindrome word");
+
+
+
+
+
+    }}
+
+
+
+
+
